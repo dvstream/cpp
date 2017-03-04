1bd86381682fcb5cbe2d9f29e827dbb5ac114fb8
Eric Zhang <Eric Zhang>
2017-03-05 01:01:23 +0800

    transform tap to 4 spaces

--- test/chapter_1.cpp
+++ test/chapter_1.cpp
@@ -6,29 +6,29 @@
 
 int main()
 {
-	for (int i = 0; i < 5; ++i)
-	{
-		char *port_state = new char[3];
-		port_state[0] = 65;
-		port_state[1] = 97;
-		port_state[2] = 0;
-		for (int j = 0; j < 3; ++j)
-		{
-			std::cout << port_state[j] << std::endl;
-		}
-		delete[]port_state;
-	}
+    for (int i = 0; i < 5; ++i)
+    {
+        char *port_state = new char[3];
+        port_state[0] = 65;
+        port_state[1] = 97;
+        port_state[2] = 0;
+        for (int j = 0; j < 3; ++j)
+        {
+            std::cout << port_state[j] << std::endl;
+        }
+        delete[]port_state;
+    }
 
-	char *port_state = new char[3];
-	port_state[0] = 65;
-	port_state[1] = 97;
-	port_state[2] = 0;
-	for (int j = 0; j < 3; ++j)
-	{
-		std::cout << port_state[j] << std::endl;
-	}
-	delete[]port_state;
+    char *port_state = new char[3];
+    port_state[0] = 65;
+    port_state[1] = 97;
+    port_state[2] = 0;
+    for (int j = 0; j < 3; ++j)
+    {
+        std::cout << port_state[j] << std::endl;
+    }
+    delete[]port_state;
 
-	return 0;
+    return 0;
 }
 
